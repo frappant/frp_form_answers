83760b89fbc411b0793fdfe7c9f2d699638c57cd
Filip Hofer <f.hofer@frappant.ch>
2017-07-19 14:00:24 +0200

    [FEATURE] Several updates
    - View export instead of custom export class
    - FormEntries ar no longer showed in list module (only in Form Export Module)
    - Oneclick exporter in List View (xls)
    - Cleaning Code
    - List of all subpages, where some formEntries are saved, is shown under list view
    — List items referr to the page, in Form Export Module

--- Classes/DataExporter/DataExporter.php
+++ Classes/DataExporter/DataExporter.php
@@ -4,36 +4,14 @@ namespace Frappant\FrpFormAnswers\DataExporter;
 
 class DataExporter
 {
-    private $csvExporter = null;
-
-    private $excelExporter = null;
-
-    private $xmlExporter = null;
-
-    private $outputPath = 'php://output';
-
-    private $fileTypes = array(
-        'csv' => 'csv',
-        'xml' => 'xml',
-        'excel' => 'xlsx'
-    );
-
-    public function __construct()
+    public function getExport($rowAnswers, \Frappant\FrpFormAnswers\Domain\Model\FormEntryDemand $formEntryDemand)
     {
-        $this->csvExporter = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Frappant\\FrpFormAnswers\\DataExporter\\CsvExporter');
-        $this->excelExporter = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Frappant\\FrpFormAnswers\\DataExporter\\ExcelExporter');
-        $this->xmlExporter = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('Frappant\\FrpFormAnswers\\DataExporter\\XmlExporter');
-    }
-
-    public function getExport($exportType, $rowAnswers, $config)
-    {
-        $exportClass = strtolower($exportType)."Exporter";
         $rows = array();
         $header = array();
         $headerKeys = (array)array_values(json_decode($rowAnswers[0]->getAnswers(), 1));
 
-        if ($config['uidLabel']) {
-            $header[] = $config['uidLabel'];
+        if ($formEntryDemand->getUidLabel()) {
+            $header[] = $formEntryDemand->getUidLabel();
         }
 
         foreach ($headerKeys as $field => $val) {
@@ -43,8 +21,8 @@ class DataExporter
         }
 
         foreach ($rowAnswers as $key => $entry) {
-            if ($config['uidLabel']) {
-                $rows[$entry->getUid()][$config['uidLabel']] = $entry->getUid();
+            if ($formEntryDemand->getUidLabel()) {
+                $rows[$entry->getUid()][$formEntryDemand->getUidLabel()] = $entry->getUid();
             }
             foreach ((array)json_decode($entry->getAnswers(), true) as $fieldName => $field) {
                 if (!preg_match('/^fieldset/', $fieldName) && !preg_match('/^statictext/', $fieldName)) {
@@ -55,30 +33,6 @@ class DataExporter
 
         array_unshift($rows, $header);
 
-        $this->downloadSendHeaders($config['fileName'].".".$this->fileTypes[strtolower($exportType)], ($config['charset'] ? $config['charset'] : null));
-
-        // Get File Content
-        $this->$exportClass->export($rows, $config);
-    }
-
-
-    private function downloadSendHeaders($filename, $charset= "UTF-8")
-    {
-        // disable caching
-        $now = gmdate("D, d M Y H:i:s");
-        header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
-        header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
-        header("Last-Modified: {$now} GMT");
-
-        header("Content-Type: application/force-download");
-        header("Content-Type: application/download; charset=$charset");
-        if (preg_match('/.csv/', $filename)) {
-            header('Content-Encoding: '.$charset);
-            header('Content-type: text/csv; charset='.$charset);
-        }
-
-        // disposition / encoding on response body
-        header("Content-Disposition: attachment;filename={$filename}");
-        header("Content-Transfer-Encoding: binary");
+        return $rows;
     }
 }
--- Classes/Domain/Repository/FormEntryRepository.php
+++ Classes/Domain/Repository/FormEntryRepository.php
@@ -1,6 +1,12 @@
 <?php
 namespace Frappant\FrpFormAnswers\Domain\Repository;
 
+use TYPO3\CMS\Core\Database\QueryGenerator;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\QueryResult;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+use Frappant\FrpFormAnswers\Utility\BackendUtility;
+
 /***
  *
  * This file is part of the "Form Answer Saver" Extension for TYPO3 CMS.
@@ -17,39 +23,73 @@ namespace Frappant\FrpFormAnswers\Domain\Repository;
  */
 class FormEntryRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 {
+    public function initializeObject()
+    {
+        $querySettings = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Typo3QuerySettings');
+
+        $querySettings->setStoragePageIds(array((int)\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('id')));
+
+        $this->setDefaultQuerySettings($querySettings);
+    }
     /**
      * Finds all FormEntries given by conf Array
      * @param  [type] $config [description]
      * @return [type]         [description]
      */
-    public function findByConfig($config)
+    public function findByDemand(\Frappant\FrpFormAnswers\Domain\Model\FormEntryDemand $formEntryDemand)
     {
-        $querySettings = $this->objectManager->get('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\Typo3QuerySettings');
-
-        if ($config['allPids']) {
-            $querySettings->setRespectStoragePage(false);
-        } else {
-            $querySettings->setRespectStoragePage(true);
-            $querySettings->setStoragePageIds(array((int)\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('id')));
-        }
-
-        $this->setDefaultQuerySettings($querySettings);
-
         $query = $this->createQuery();
+
+        if ($formEntryDemand->getAllPids()) {
+            $query->getQuerySettings()->setRespectStoragePage(false);
+        }
 
         $constraints = array();
 
-        if (!$config['selectAll']) {
+        if (!$formEntryDemand->getSelectAll()) {
             $constraints[] = $query->equals('exported', false);
         }
 
-        if ($config['form']) {
-            $constraints[] = $query->equals('fieldHash', $config['form']);
+        if ($formEntryDemand->getForm()) {
+            $constraints[] = $query->equals('fieldHash', $formEntryDemand->getForm());
+        }
+
+        if ($formEntryDemand->getFormName()) {
+            $constraints[] = $query->equals('form', $formEntryDemand->getFormName());
         }
 
         if (count($constraints)) {
             $query->matching($query->logicalAnd($constraints));
         }
+        return $query->execute();
+    }
+
+    /**
+     * Find all within a Page and all subpages
+     *
+     * @param int $pid start page identifier
+     * @return QueryResult
+     */
+    public function findAllInPidAndRootline($pid)
+    {
+        $query = $this->createQuery();
+        $query->getQuerySettings()->setRespectStoragePage(false);
+
+        if ($pid > 0) {
+            $queryGenerator = GeneralUtility::makeInstance(QueryGenerator::class);
+            $pids = GeneralUtility::trimExplode(',', $queryGenerator->getTreeList($pid, 20, 0, 1), true);
+            $pids = BackendUtility::filterPagesForAccess($pids);
+            $query->matching($query->in('pid', $pids));
+        } else {
+            if (!BackendUtility::isBackendAdmin()) {
+                $pageRepository = GeneralUtility::makeInstance(PageRepository::class);
+                $pids = $pageRepository->getAllPages();
+                $pids = BackendUtility::filterPagesForAccess($pids);
+                $query->matching($query->in('pid', $pids));
+            }
+        }
+        $query->setOrderings(['pid' => QueryInterface::ORDER_ASCENDING]);
+
         return $query->execute();
     }
 }
