efb04d160a9b64077498302c902f5230ba65733c
Maciej Szklarczyk <maciej.szlkarczyk@drblitz-weblab.com>
2017-12-20 10:10:32 +0100

    Scheduler mailing command

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,3 +4,6 @@ defined('TYPO3_MODE') || die('Access denied.');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
         mod.web_list.hideTables = tx_frpformanswers_domain_model_formentry
 ');
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][$_EXTKEY . '_1'] =
+    \Frappant\FrpFormAnswers\Command\MailAdminNotificationCommandController::class;
