4e5f54f7c68e6120f2eb9d33dbcec004be52ce80
Hofer Filip <f.hofer@frappant.ch>
2020-05-20 12:37:42 +0200

    [TASK] Version constraint to Typo3 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
