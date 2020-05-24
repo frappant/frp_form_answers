d863258f2f2dc44684958f2145b765aae3947ca0
Hofer Filip <f.hofer@frappant.ch>
2020-05-24 16:22:55 +0200

    [TASK] Add formAnswersJsonElement user field class

---
+++ Classes/Form/FormAnswersJsonElement.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Frappant\FrpFormAnswers\Form;
+
+use TYPO3\CMS\Backend\Form\Element\AbstractFormElement;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
+class FormAnswersJsonElement extends AbstractFormElement
+{
+    public function render()
+    {
+        // Custom TCA properties and other data can be found in $this->data, for example the above
+        // parameters are available in $this->data['parameterArray']['fieldConf']['config']['parameters']
+        $resultArray = $this->initializeResultArray();
+        $fieldValues = json_decode($this->data['databaseRow']['answers'], true);
+
+        $out = '<ul>';
+
+        if (is_array($fieldValues)) {
+            foreach ($fieldValues as $fieldKey => $fieldValue) {
+                if ($fieldValue['conf']['label']) {
+                    $out .= '<li>'.$fieldValue['conf']['label'].' - '.(is_array($fieldValue['value']) ? implode(",", $fieldValue['value']) : $fieldValue['value']).'</li>';
+                } else {
+                    $out .= '<li>'.$fieldKey.' - '.(is_array($fieldValue['value']) ? implode(",", $fieldValue['value']) : $fieldValue['value']).'</li>';
+                }
+            }
+        }
+        $out .= '</ul>';
+
+        $resultArray['html'] = $out;
+        return $resultArray;
+    }
+}
