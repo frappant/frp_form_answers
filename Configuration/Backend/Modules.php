5ee9a9569665c1bd1c3a230aacdaebef164363fe
Yannick Gafner <yannick-gafner@bluewin.ch>
2023-09-22 14:22:13 +0200

    [TASK] working on update

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'web_examples' => [
+    'frp_form_answers' => [
         'parent' => 'web',
         'position' => ['after' => 'web_info'],
         'access' => 'user',
         'workspaces' => 'live',
         'path' => '/module/page/formanswers',
         'labels' => 'LLL:EXT:frp_form_answers/Resources/Private/Language/locallang_formanswers.xlf',
-        'extensionName' => 'frp-form-answers',
+        'extensionName' => 'frp_form_answers',
         'controllerActions' => [
             \Frappant\FrpFormAnswers\Controller\FormEntryController::class => [
-                'show',
                 'list',
+                'show',
                 'prepareRemove',
                 'remove',
                 'prepareExport',
