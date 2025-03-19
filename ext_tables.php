7abf48709e5e9b5b0c5eed8756ce2555a7c52c4d
Yannick Gafner <y.gafner@frappant.ch>
2025-03-19 13:10:59 +0100

    [TASK] working on update

--- ext_tables.php
+++ ext_tables.php
@@ -2,9 +2,11 @@
 use TYPO3\CMS\Core\Http\ApplicationType;
 
 defined('TYPO3') or die();
-call_user_func(
-    function () {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_frpformanswers_domain_model_formentry', 'EXT:frp_form_answers/Resources/Private/Language/locallang_csh_tx_frpformanswers_domain_model_formentry.xlf');
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_frpformanswers_domain_model_formentry');
-    }
-);
+
+$GLOBALS['TCA_DESCR']['tx_frpformanswers_domain_model_formentry'] = [
+    'refs' => [
+        'EXT:frp_form_answers/Resources/Private/Language/locallang_csh_tx_frpformanswers_domain_model_formentry.xlf',
+    ],
+];
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_frpformanswers_domain_model_formentry');
