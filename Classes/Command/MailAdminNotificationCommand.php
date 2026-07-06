9cf2e9d4ef53992165c4595181df519ecd8b3a70
Jon <jon@web-vision.de>
2026-07-06 15:47:49 +0200

    [TASK] Update MailAdminNotificationCommand for TYPO3 14

--- Classes/Command/MailAdminNotificationCommand.php
+++ Classes/Command/MailAdminNotificationCommand.php
@@ -1,173 +1,166 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Frappant\FrpFormAnswers\Command;
 
-use TYPO3\CMS\Core\Mail\MailMessage;
+use Frappant\FrpFormAnswers\Domain\Model\FormEntry;
 use Frappant\FrpFormAnswers\Domain\Model\FormEntryDemand;
 use Frappant\FrpFormAnswers\Domain\Repository\FormEntryRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use TYPO3\CMS\Core\Exception;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Mime\Address;
+use TYPO3\CMS\Core\Mail\MailerInterface;
+use TYPO3\CMS\Core\Mail\MailMessage;
+use TYPO3\CMS\Core\View\ViewFactoryData;
+use TYPO3\CMS\Core\View\ViewFactoryInterface;
 use TYPO3\CMS\Extbase\Persistence\Exception\IllegalObjectTypeException;
 use TYPO3\CMS\Extbase\Persistence\Exception\UnknownObjectException;
-use TYPO3\CMS\Fluid\View\StandaloneView;
-use Frappant\FrpFormAnswers\Domain\Model\FormEntry;
-use Symfony\Component\Console\Command\Command;
+use TYPO3\CMS\Extbase\Persistence\PersistenceManagerInterface;
 
-class MailAdminNotificationCommand extends Command
+#[AsCommand(
+    name: 'f:formanswers:mailNotification',
+    description: 'Sends a notification mail with a list of not exported form entries.',
+)]
+final class MailAdminNotificationCommand extends Command
 {
-
-    /**
-     * FormEntryRepository
-     *
-     * @var FormEntryRepository
-     */
-    protected $formEntryRepository;
-
-    /**
-     * Inject FormEntryRepository
-     *
-     * @param FormEntryRepository $pageRepository
-     */
-    public function injectFormEntryRepository(FormEntryRepository $formEntryRepository)
-    {
-        $this->formEntryRepository = $formEntryRepository;
+    public function __construct(
+        private readonly FormEntryRepository $formEntryRepository,
+        private readonly ViewFactoryInterface $viewFactory,
+        private readonly MailerInterface $mailer,
+        private readonly PersistenceManagerInterface $persistenceManager,
+    ) {
+        parent::__construct();
     }
 
-	/**
-	 * Configure the command by defining the name, options and arguments.
-	 */
-	protected function configure()
-	{
-		$this
-			->setDescription('Sends a notification mail with a list of not exportet form entries.')
-			->addArgument(
-			'mailto',
-				InputArgument::REQUIRED,
-			'E-Mail Address the Mail should be sent to.'
-			)
-			->addOption(
-				'formname',
-						'',
-				InputOption::VALUE_OPTIONAL,
-				'Name of the form to be checked.'
-			)
-			->addOption(
-			'title',
-				'',
-			InputOption::VALUE_OPTIONAL,
-			'Subject Label.'
-			);
-	}
-
-    /**
-     * @param $mails
-     * @return string
-     */
-    public function generateMailBody($mails)
+    protected function configure(): void
     {
-        // Rendering of the output via fluid
-        $view = GeneralUtility::makeInstance(StandaloneView::class);
-        $view->setFormat('html');
-        $templateRootPath = GeneralUtility::getFileAbsFileName(
-            'EXT:frp_form_answers/Resources/Private/CommandTask/Templates'
-        );
-        $partialRootPaths = GeneralUtility::getFileAbsFileName(
-            'EXT:frp_form_answers/Resources/Private/CommandTask/Partials'
-        );
-        $layoutRootPaths = GeneralUtility::getFileAbsFileName(
-            'EXT:frp_form_answers/Resources/Private/CommandTask/Layouts'
-        );
-        $view->setTemplateRootPaths(array($templateRootPath));
-        $view->setPartialRootPaths(array($partialRootPaths));
-        $view->setLayoutRootPaths(array($layoutRootPaths));
-        $view->setTemplatePathAndFilename(
-            GeneralUtility::getFileAbsFileName(
-                'EXT:frp_form_answers/Resources/Private/CommandTask/Templates/FormEntries/InMail.html'
+        $this
+            ->addArgument(
+                'mailto',
+                InputArgument::REQUIRED,
+                'Email address the mail should be sent to. Multiple addresses can be comma-separated.'
             )
-        );
-        $view->assignMultiple(['mails' => $mails]);
-        return $view->render();
+            ->addOption(
+                'formname',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Name of the form to be checked.'
+            )
+            ->addOption(
+                'title',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Subject label.'
+            );
     }
 
-	/**
-	 * Email notification about sent forms.
-	 *
-	 * @param InputInterface $input
-	 * @param OutputInterface $output
-	 * @throws Exception
-	 */
-    public function execute(InputInterface $input, OutputInterface $output): int
+    private function generateMailBody(iterable $mails): string
     {
-    	$mailto = $input->getArgument('mailto');
-    	$formname = $input->getOption('formname');
-	    $title= $input->getOption('title');
+        $viewFactoryData = new ViewFactoryData(
+            templateRootPaths: [
+                'EXT:frp_form_answers/Resources/Private/CommandTask/Templates',
+            ],
+            partialRootPaths: [
+                'EXT:frp_form_answers/Resources/Private/CommandTask/Partials',
+            ],
+            layoutRootPaths: [
+                'EXT:frp_form_answers/Resources/Private/CommandTask/Layouts',
+            ],
+        );
 
-        if (empty($mailto)) {
-            throw new Exception('You need to provide at least one email address.', 2340297863);
+        $view = $this->viewFactory->create($viewFactoryData);
+        $view->assignMultiple([
+            'mails' => $mails,
+        ]);
+
+        return $view->render('FormEntries/InMail');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $mailto = trim((string)$input->getArgument('mailto'));
+        $formname = trim((string)($input->getOption('formname') ?? ''));
+        $title = trim((string)($input->getOption('title') ?? ''));
+
+        $recipients = array_values(array_filter(
+            array_map('trim', explode(',', $mailto)),
+            static fn (string $email): bool => $email !== ''
+        ));
+
+        if ($recipients === []) {
+            throw new \RuntimeException(
+                'You need to provide at least one email address.',
+                2340297863
+            );
         }
 
-        // $formEntryRepository = GeneralUtility::makeInstance(ObjectManager::class)->get(FormEntryRepository::class);
-
-        $search = GeneralUtility::makeInstance(FormEntryDemand::class);
+        $search = new FormEntryDemand();
         $search->setAllPids(true);
 
-        if ($formname) {
-        	$output->writeln("Searching for form ".$formname);
+        if ($formname !== '') {
+            $io->writeln('Searching for form ' . $formname);
             $search->setFormName($formname);
-        }else{
-	        $output->writeln("Searching for no specific form");
-        }
-
-        $frommail = $GLOBALS['TYPO3_CONF_VARS']['MAIL']['defaultMailFromAddress'];
-        if (!empty($frommail)) {
-        	$output->writeln("Default E-Mail Address: ".$frommail);
-            $from = $frommail;
         } else {
-            throw new Exception("['TYPO3_CONF_VARS']['MAIL']['defaultMailFromAddress'] is not set.", 5763504134);
+            $io->writeln('Searching for no specific form');
         }
-        $records = $this->formEntryRepository->findByDemand($search);
 
-        if ($records->count()) {
-        	$output->writeln($records->count()." entries found");
+        $fromMail = (string)($GLOBALS['TYPO3_CONF_VARS']['MAIL']['defaultMailFromAddress'] ?? '');
+        $fromName = (string)($GLOBALS['TYPO3_CONF_VARS']['MAIL']['defaultMailFromName'] ?? '');
+
+        if ($fromMail === '') {
+            throw new \RuntimeException(
+                '$GLOBALS[\'TYPO3_CONF_VARS\'][\'MAIL\'][\'defaultMailFromAddress\'] is not set.',
+                5763504134
+            );
+        }
+
+        $io->writeln('Default email address: ' . $fromMail);
+
+        $records = $this->formEntryRepository->findByDemand($search);
+        $recordCount = $records->count();
+
+        if ($recordCount === 0) {
+            $io->writeln('Nothing to send.');
+            return Command::SUCCESS;
+        }
+
+        $io->writeln($recordCount . ' entries found');
+
+        $body = $this->generateMailBody($records);
+        $subject = $title !== '' ? $title : 'Scheduler mails update ' . date('d/m/y');
+
+        foreach ($recipients as $recipient) {
+            $email = (new MailMessage())
+                ->subject($subject)
+                ->from(new Address($fromMail, $fromName))
+                ->to(Address::create($recipient))
+                ->html($body);
+
+            $this->mailer->send($email);
+        }
+
+        try {
             /** @var FormEntry $row */
             foreach ($records as $row) {
-            	$output->writeln("Sending entry with uid:".$row->getUid());
+                $io->writeln('Marking entry as exported, uid: ' . $row->getUid());
                 $row->setExported(true);
-                try {
-	                $this->formEntryRepository->update($row);
-                } catch (IllegalObjectTypeException $e) {
-                	$output->writeln($e->getMessage());
-                    return 0;
-                } catch (UnknownObjectException $e) {
-	                $output->writeln($e->getMessage());
-                    return 0;
-                }
+                $this->formEntryRepository->update($row);
             }
-            $body = $this->generateMailBody($records);
-            $date = date("d/m/y");
-            if (!empty($title)) {
-                $subject = $title;
-            } else {
-                $subject = "Scheduler mails update " . $date;
-            }
-            $trim = GeneralUtility::trimExplode(',', $mailto, 1);
-            foreach ($trim as $singlemail) {
-                $mail = GeneralUtility::makeInstance(MailMessage::class);
-                $mail
-                    ->setSubject($subject)
-                    ->setFrom(array($from))
-                    ->setTo(array($singlemail))
-                    ->setBody($body, 'text/html')
-                    ->send();
-            }
-        } else {
-            $output->writeln("Nothing to send.");
-        }
-        return 1;
-    }
 
+            $this->persistenceManager->persistAll();
+        } catch (IllegalObjectTypeException | UnknownObjectException $e) {
+            $io->error($e->getMessage());
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
 }
