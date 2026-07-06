6670fcc2b15efd42f027e5b85a58dcf2152483f0
Jon <jon@web-vision.de>
2026-07-06 12:43:17 +0200

    [TASK] Remove deprecated use of addPageTSConfig, already defined under `web_list.tsconfig`

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,5 @@
 <?php
 defined('TYPO3') or die();
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-        mod.web_list.hideTables = tx_frpformanswers_domain_model_formentry
-');
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1590321374] = [
     'nodeName' => 'formAnswersJsonElement',
